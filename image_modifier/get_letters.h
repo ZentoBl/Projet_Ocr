f51c8681d5852340f6a62cf76ae11e7796270022
Noé <no.bredif@epita.fr>
2025-10-31 19:29:28 +0100

    grid estimation

--- image_modifier/get_letters.h
+++ image_modifier/get_letters.h
@@ -4,16 +4,34 @@
 
 typedef struct {
     int x, y, w, h;
-} BoundingBox;
+    size_t cx, cy; // center
+    size_t nb_pixels;
+} Box;
 
 typedef struct {
-    int row, col;
-    BoundingBox box;
+    Box box;
+    size_t letter_idx;
+    size_t word_id;
+} WordLetter;
+
+typedef struct {
+    Box box;
+    size_t row, col;
 } GridLetter;
 
+size_t detect_connected_components(SDL_Surface *img, Box *boxes, size_t max_boxes, int **out_segments);
 
-void fill(SDL_Surface *img, int x, int y, char *visited, int *x_min, int *x_max, int *y_min, int *y_max);
+void save_boxes(SDL_Surface *img, Box *boxes, size_t nb, char *parent_name);
 
-size_t detect_connected(SDL_Surface *img, BoundingBox *boxes, size_t max_boxes);
+void save_grid_letters(SDL_Surface *img, GridLetter *gl, size_t nb, const char *parent_name);
+void save_word_letters(SDL_Surface *img, WordLetter *wl, size_t nb, const char *parent_name);
 
-void save_letters(SDL_Surface *img, char *parent_name);
+size_t split_large_boxes(SDL_Surface *img, Box *boxes, size_t n, size_t max_boxes);
+
+// cluster rows by cy, then cols, assign row/col
+size_t group_letters_into_grid(SDL_Surface *img, Box *boxes, size_t n, GridLetter *out_letters, size_t max_letters, size_t *out_row_count, size_t *out_col_count);
+
+// group by cy within tol_y, then sort by cx
+size_t group_letters_into_words(Box *boxes, size_t n, WordLetter *out_letters, size_t max_letters, size_t *out_words_count);
+
+void save_with_marks(SDL_Surface *img, Box *boxes, size_t *indices, size_t nx, size_t ny, const char *parent_name);
--- image_modifier/main.c
+++ image_modifier/main.c
@@ -1,20 +1,19 @@
 #include <err.h>
+#include <SDL2/SDL.h>
 #include "preprocess.h"
 #include "get_letters.h"
+
+#define MAX_BOXES 2048
 
 int main(int argc, char **argv)
 {
     if (argc > 2)
         errx(EXIT_FAILURE, "argc = %i, used $ %s <inputfile> <outputfile>", argc, argv[0]);
-    char *name_file;
-    if (argc == 2)
-        name_file = argv[1];
-    else
-        name_file = "level_1_image_1.png";
+    const char *name_file = (argc == 2) ? argv[1] : "level_1_image_1.png";
 
     if (SDL_Init(SDL_INIT_VIDEO) != 0)
         errx(EXIT_FAILURE, "SDL_Init failed");
-    
+
     char input[64] = "";
     sprintf(input, "tests_images/%s", name_file);
     SDL_Surface *img = load_img(input);
@@ -26,8 +25,35 @@ int main(int argc, char **argv)
     sprintf(black_white_name, "black_and_white/%s_black_white.bmp", name_file);
     save_img_bmp(img, black_white_name);
 
-    save_letters(img, name_file);
+    Box *boxes = malloc(sizeof(Box) * MAX_BOXES);
+    int *segments;
+    size_t nb = detect_connected_components(img, boxes, MAX_BOXES, &segments);
+    printf("%li boxes detected\n", nb);
+    
+    nb = split_large_boxes(img, boxes, nb, MAX_BOXES);
+    printf("%li letters detected\n", nb);
 
+    save_boxes(img, boxes, nb, input);
+    
+    GridLetter *gl = malloc(sizeof(GridLetter) * MAX_BOXES);
+    size_t rows, cols;
+    size_t ngl = group_letters_into_grid(img, boxes, nb, gl, MAX_BOXES, &rows, &cols);
+    printf("Grid detected: %li letters, rows=%li cols=%li\n", ngl, rows, cols);
+    save_grid_letters(img, gl, ngl, name_file);
+
+/*
+    WordLetter *wl = malloc(sizeof(WordLetter) * MAX_BOXES);
+    size_t words_count;
+    size_t nwl = group_letters_into_words(boxes, nb, wl, MAX_BOXES, &words_count);
+    printf("Found %li word-letters in %li words \n", nwl, words_count);
+    save_word_letters(img, wl, nb, name_file);
+*/
+
+    free(segments);
+    free(boxes);
+    free(gl);
+    //free(wl);
+    
     SDL_FreeSurface(img);
     SDL_Quit();
     return EXIT_SUCCESS;
